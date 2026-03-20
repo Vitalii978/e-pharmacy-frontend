b093e99b9458b0597f7be224cb0ccb7f5eb413c0
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2026-03-20 13:45:20 +0100

    Fixed the position of the logoutBtn button in the burger menu.

--- src/components/MobileMenu/NavMenu/NavMenu.jsx
+++ src/components/MobileMenu/NavMenu/NavMenu.jsx
@@ -1,35 +1,3 @@
-// // ============================================
-// // NavMenu.jsx - МЕНЮ С ИКОНКАМИ (ИСПОЛЬЗУЕТСЯ И В SIDEBAR, И В МОБИЛЬНОМ МЕНЮ)
-// // ============================================
-
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import sprite from '../../../assets/sprite.svg';
-// import { menuInfo } from '../../../lib/menu';
-// import './NavMenu.css';
-
-// const NavMenu = () => {
-//   return (
-//     <ul className="nav-menu-list">
-//       {/* Проходим по массиву menuInfo и создаем пункты меню */}
-//       {menuInfo?.map((item, index) => (
-//         <li key={index} className="nav-menu-item">
-//           {/* NavLink - специальная ссылка из react-router-dom */}
-//           {/* Она автоматически добавляет класс "active" на текущей странице */}
-//           <NavLink to={item.to} className="nav-menu-link">
-//             <svg width={14} height={14}>
-//               {/* Используем иконку из спрайта по id */}
-//               <use xlinkHref={`${sprite}#${item.icon}`} />
-//             </svg>
-//           </NavLink>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default NavMenu;
-
 // ============================================
 // NavMenu.jsx - МЕНЮ С ИКОНКАМИ
 // ============================================
